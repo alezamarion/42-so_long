9135e012663c791ee735ddfa92c15ed29332704c
alezamarion <alezamarion@icloud.com>
2021-09-23 20:25:02 -0300

    few changes

--- src/so_long.c
+++ src/so_long.c
@@ -6,9 +6,10 @@
 /*   By: azamario <azamario@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/23 15:43:47 by azamario          #+#    #+#             */
-/*   Updated: 2021/09/23 18:18:37 by azamario         ###   ########.fr       */
+/*   Updated: 2021/09/23 18:21:45 by azamario         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "so_long.h"
 
+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
